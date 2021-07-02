061a1cdc367dff1704f64c8ca8acbe8cbd0afe41
kim-shun <kimura1417080@gmail.com>
2021-07-02 09:29:01 +0900

    submitを取得

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -2,7 +2,7 @@ function post(){
   const submit = document.getElementById("submit_btn");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("submit_btn")
+    console.log(submit);
   });
 };
 
